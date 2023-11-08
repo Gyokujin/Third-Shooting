4ab784248b32b0cc59085fc1eff017f06ecc3d45
Gyokujin <dd43454548@naver.com>
2023-11-08 11:47:01 +0900

    # 조준 기능 업데이트

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -13,6 +13,15 @@ public class PlayerManager : MonoBehaviour
     private CinemachineVirtualCamera aimCam;
     [SerializeField]
     private GameObject aimImage;
+    [SerializeField]
+    private float aimSpeed = 50f;
+    [SerializeField]
+    private GameObject aimObject;
+    [SerializeField]
+    private float aimObjDis = 10f;
+
+    [SerializeField]
+    private LayerMask targetLayer;
 
     void Start()
     {
@@ -30,6 +39,27 @@ public class PlayerManager : MonoBehaviour
         {
             aimCam.gameObject.SetActive(true);
             aimImage.SetActive(true);
+
+            Vector3 targetPosition = Vector3.zero;
+            Transform camTransform = Camera.main.transform;
+            RaycastHit hit;
+
+            if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, Mathf.Infinity, targetLayer))
+            {
+                targetPosition = hit.point;
+                aimObject.transform.position = hit.point;
+            }
+            else
+            {
+                targetPosition = camTransform.position + camTransform.forward * aimObjDis;
+                aimObject.transform.position = camTransform.position + camTransform.forward * aimObjDis;
+            }
+
+            Vector3 targetAim = targetPosition;
+            targetAim.y = transform.position.y;
+            Vector3 aimDir = (targetAim - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDir, Time.deltaTime * aimSpeed);
         }
         else
         {
