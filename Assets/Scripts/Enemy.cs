6474302d00c10078c0da2220a2be61b7510a50a8
Gyokujin <dd43454548@naver.com>
2023-12-07 05:09:07 +0900

    # NavMesh 및 좀비 AI 업데이트

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
@@ -10,18 +12,71 @@ public class Enemy : MonoBehaviour
     private float enemyMaxHP = 10;
     public float enemyCurrentHP = 0;
 
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    private GameObject targetPlayer;
+    private float targetDelay = 0.5f;
+
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        targetPlayer = GameObject.FindWithTag("Player");
+
         InitEnemyHP();
     }
 
     void Update()
     {
         HPBar.value = enemyCurrentHP / enemyMaxHP;
+
+        if (enemyCurrentHP <= 0)
+        {
+            StartCoroutine(EnemyDie());
+            return;
+        }
+        
+        if (targetPlayer != null)
+        {
+            float maxDelay = 0.5f;
+            targetDelay += Time.deltaTime;
+
+            if (targetDelay <= maxDelay)
+            {
+                return;
+            }
+
+            agent.destination = targetPlayer.transform.position;
+            transform.LookAt(targetPlayer.transform.position);
+
+            bool isRange = Vector3.Distance(transform.position, targetPlayer.transform.position) <= agent.stoppingDistance;
+
+            if (isRange)
+            {
+                animator.SetTrigger("Attack");
+            }
+            else
+            {
+                animator.SetFloat("MoveSpeed", agent.velocity.magnitude);
+            }
+
+            targetDelay = 0;
+        }
     }
 
     void InitEnemyHP()
     {
         enemyCurrentHP = enemyMaxHP;
+    }
+
+    IEnumerator EnemyDie()
+    {
+        agent.speed = 0;
+        animator.SetTrigger("Dead");
+
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
     }
 }
